78c8f5bdb629586f4d0012413c2db89f6ba7b9bf
Tri Pham <tripham178@gmail.com>
2017-11-19 15:08:57 +0700

    add animate open match for prediction, livescore

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,12 +1,16 @@
 import MobileMenu from './modules/MobileMenu';
 import DesktopMenu from './modules/DesktopMenu';
-import MobilePredictionDetail from './modules/MobilePredictionDetail';
+
+import Prediction from './modules/Prediction';
 import StatsLiveStreamClick from './modules/StatsLiveStreamClick';
+import LiveScore from './modules/LiveSocre';
+
+var livescore = new LiveScore();
 
 var mobileMenu = new MobileMenu();
 
 var desktopMenu = new DesktopMenu();
 
-var mobilePredictionDetail = new MobilePredictionDetail();
+var prediction = new Prediction();
 
 var statsLiveStreamClick = new StatsLiveStreamClick();
